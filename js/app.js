e0f398fe51b9383709454fc46f27f30db65da709
AlessandroBaldisser <alessandro.baldisser.01@gmail.com>
2024-07-02 18:55:47 +0200

    ADD: some context to calculations

--- js/app.js
+++ js/app.js
@@ -9,10 +9,15 @@ console.log("Prezzo biglietto " + prezzoBiglietto + "$");
 // se il cliente è minorenne ha uno sconto del 20% se invece è over 65 ha uno sconto del 40%
 let prezzoBigliettoScontato = document.getElementById("prezzoScontato")
 
+/*ho utilizzato .innerHTML per mandare il contenuto a schermo, 
+ho utilizzato invece la funzione .toFixed(2) per arrotondare a due cifre decimali*/
 if (etaCliente < 18){
     let sconto = prezzoBiglietto * 0.2;
-    prezzoBigliettoScontato.innerHTML = prezzoBiglietto - sconto;
+    prezzoBigliettoScontato.innerHTML = ((prezzoBiglietto - sconto).toFixed(2) + " euro");
 }   else if(etaCliente >= 65){
     let sconto = prezzoBiglietto * 0.4;
-    prezzoBigliettoScontato.innerHTML = prezzoBiglietto - sconto;
+    prezzoBigliettoScontato.innerHTML = ((prezzoBiglietto - sconto).toFixed(2) + " euro");
 }
+
+
+
